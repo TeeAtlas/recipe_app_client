36621ef14c009ac81dddad278d92eb3b0c0002bf
Tania Boterman <taniaboterman@gmail.com>
2024-02-04 21:50:10 +0100

    changes need to be made to accomodate new recipe table

--- src/views/useRecipeAPI.jsx
+++ src/views/useRecipeAPI.jsx
@@ -1,6 +1,9 @@
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const useRecipeAPI = () => {
+    const [recipe, setRecipe] =  useState(null);
+
     const getRecipes = async () => {
         try{
             const response = await axios.get('http://localhost:8000');
